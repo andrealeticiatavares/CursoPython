67855de50c7db69d5fa7ab29e7ed17bac9e745f5
Andréa Letícia de Sousa Tavares <andrealeticiatavares@gmail.com>
2024-05-25 17:31:16 -0300

    feat: aula 02 - Variáveis e tipos de dados

---
+++ Aula02/app.py
@@ -0,0 +1,23 @@
+# Váriaveis
+nome = "Manu"
+idade = "13"
+gosto_de = "jogar video game"
+
+# Concatenar: juntar strings
+print("Meu nome é " + nome + ".")
+
+# f-string: forma eficiente de formatar strings em Python, adicionando váriaveis dentro da string.
+print(f"Meu nome é {nome}.")
+print(f"Tenho {idade} anos.")
+print(f"Gosto de {gosto_de}.")
+
+# Tipos de váriaveis
+nome = "Roger" # Texto (String)
+idade = 21     # Inteiro (int)
+altura = 1.78  # Ponto flutuante (float)
+homem = True   # Boolean (verdadeiro / falso)
+
+print(f"Meu nome é {nome}.")
+print(f"Tenho {idade} anos.")
+print(f"Minha altura é {altura} metros.")
+print(f"É {homem} que sou homem.")
